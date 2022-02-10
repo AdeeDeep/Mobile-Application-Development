eaf0319d18917145f3968cb34b738aa760a1369c
AdeeDeep <74358752+AdeeDeep@users.noreply.github.com>
2022-02-10 15:18:03 +0530

    Update MainActivity.java

--- Exercise 15/MainActivity.java
+++ Exercise 15/MainActivity.java
@@ -1,8 +1,12 @@
-package com.example.myapplication;
+package com.example.intent2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button b1=(Button)findViewById(R.id.b1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(Intent.ACTION_VIEW, Uri.parse("https://google.com"));
+                startActivity(i);
+            }
+        });
     }
 }

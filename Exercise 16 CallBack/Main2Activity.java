0375031511b4db3f329395b2ce30563a0f7dc82e
AdeeDeep <74358752+AdeeDeep@users.noreply.github.com>
2022-03-01 11:38:59 +0530

    Update Main2Activity.java

--- Exercise 16 CallBack/Main2Activity.java
+++ Exercise 16 CallBack/Main2Activity.java
@@ -1,4 +1,4 @@
-package com.example.callback;
+package com.adee.callbackz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,21 +8,26 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class Main2Activity extends AppCompatActivity {
-
+public class MainActivity2 extends AppCompatActivity {
+Button b1;
+EditText e;
+String s;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        final EditText e=(EditText)findViewById(R.id.edit);
-        Button b=(Button)findViewById(R.id.ok);
-        b.setOnClickListener(new View.OnClickListener() {
+        b1=(Button)findViewById(R.id.ok);
+        e=(EditText)findViewById(R.id.e1);
+        b1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent i=new Intent(Main2Activity.this,MainActivity.class);
-                i.putExtra("msg",e.getText().toString());
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity2.this,MainActivity.class);
+                s=e.getText().toString();
+                i.putExtra("msg",s);
                 startActivity(i);
+
             }
         });
+
     }
 }
